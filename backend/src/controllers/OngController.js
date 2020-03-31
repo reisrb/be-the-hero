eacdc7d87cfd5352ebea6a65b56a23146754e66b
reisrb <rafael.reis@bandtec.com.br>
2020-03-31 14:24:04 -0300

    Front-end do app

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -3,26 +3,26 @@ const crypto = require('crypto');
 
 module.exports = {
 
-    async index (requst, response) {
-        const ongs = await connection('ongs').select('*');
-    
-        return response.json(ongs);
-    },
+  async index(requst, response) {
+    const ongs = await connection('ongs').select('*');
 
-    async create(request, response) {
-        const { name, email, whatsapp, city, uf } = request.body;
+    return response.json(ongs);
+  },
 
-        const id = crypto.randomBytes(4).toString('HEX');
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf } = request.body;
 
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf
-        });
+    const id = crypto.randomBytes(4).toString('HEX');
 
-        return response.json({ id });
-    }
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return response.json({ id });
+  }
 }
